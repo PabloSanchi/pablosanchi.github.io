ec931e2a068108d487d02944a28514ce1b2c620c
Pablo <pablo.sanchi.herrera@gmail.com>
2022-09-05 16:59:59 +0200

    files

---
+++ app.js
@@ -0,0 +1,30 @@
+const $ = (name) => document.querySelector(name);
+btn = $("#btn-doc");
+doc = $("#doc");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (doc.style.display == "none") {
+    doc.style.display = "block";
+    doc.style.width = "100%";
+    doc.style.height = "19%";
+    doc.style.padding = "2rem";
+    btn.innerHTML = "Hide";
+  } else {
+    doc.style.display = "none";
+    btn.innerHTML = "Show";
+  }
+});
+
+// document.onscroll = function () {
+//   var pos = getVerticalScrollPercentage(document.body);
+//   // document.body.innerHTML = "<span>" + Math.round(pos) + "%<span>"
+//   // console.log(pos);
+// };
+
+// function getVerticalScrollPercentage(elm) {
+//   var p = elm.parentNode;
+//   return (
+//     ((elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight)) * 100
+//   );
+// }
